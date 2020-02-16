d4ff05e8ebe3d7ad550d48a00ee69342340c3dff
Katie Rodgers <katierodgers95@gmail.com>
2020-02-16 12:49:09 +0000

    test: added more user sign up feature tests

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -8,4 +8,36 @@ feature "user can sign up" do
     click_button "Signup"
     expect(page).to have_content("Welcome to The Book Explorer")
   end
+
+  scenario 'wrong email address' do
+    visit('/')
+    click_link('Sign Up')
+    fill_in 'user[email]', with: 'usertwo@@gmail.com'
+    click_button "Signup"
+    expect(page).to have_content('Email is invalid')
+  end
+
+  scenario 'return error if no password is inserted' do
+    visit('/')
+    click_link('Sign Up')
+    fill_in 'user[email]', with: 'userone@gmail.com'
+    click_button 'Signup'
+    expect(page).to have_content('Password can\'t be blank')
+  end
+
+  scenario 'email is already in use' do
+    visit('/')
+    click_link('Sign Up')
+    fill_in 'user[email]', with: 'userone@gmail.com'
+    fill_in 'user[password]', with: 'secret'
+    fill_in "user[password_confirmation]", with: "secret"
+    click_button('Signup')
+    visit('/')
+    click_link('Sign Up')
+    fill_in 'user[email]', with: 'userone@gmail.com'
+    fill_in 'user[password]', with: 'secret'
+    fill_in "user[password_confirmation]", with: "secret"
+    click_button('Signup')
+    expect(page).to have_content('Email has already been taken')
+  end
 end
